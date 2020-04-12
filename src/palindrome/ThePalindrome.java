9dfc8fdf500d2c23b04bd9206823bd7925764368
AMEMIYA_KENJI_MAC <hitohahito@hotmail.co.jp>
2020-04-12 15:17:45 +0900

    デバックコード追加

--- src/palindrome/ThePalindrome.java
+++ src/palindrome/ThePalindrome.java
@@ -5,10 +5,9 @@ public class ThePalindrome {
         for(int i = s.length(); ; i++) {
             boolean flag = true;
             for(int k =0; k < s.length(); k++) {
-                // 反対側の文字が存在し、尚且つ違う文字であれば
-                // フラグを折る
-                System.out.println("k= "+k);
-                System.out.println("i= "+i);
+                // デバックコード
+                //System.out.println("k= "+k);
+                //System.out.println("i= "+i);
                 if( (i - k - 1) < s.length()
                         && s.charAt(k) != s.charAt(i - k -1)) {
                     flag = false;
